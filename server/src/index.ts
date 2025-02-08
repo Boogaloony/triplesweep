7e342a582cc2a0944b2fc6491dcd32356ba15550
Boogaloony <scott.adam.mcsweeney@gmail.com>
2025-02-09 03:22:45 +1000

    removed ts-node from production

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://triplesweep.com'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
